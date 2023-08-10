8797eace6e1cedae36131739d2a3763ca5777857
Hardnessandextreme <86927718+Hardnessandextreme@users.noreply.github.com>
2023-08-10 16:48:49 -0500

    correcion de errores en el dao
    - Se agrego el metodo esAdministrador()
    - Se retiraron variables demas
    - Se cambio el nombre de la variable que contiene la sentencia para verificar si un usuario es administrador.
    - Se modificaron las sentencias log.debug() en el metodo logearUsuario() y registrarUsuario()

--- src/dao_usuario.py
+++ src/dao_usuario.py
@@ -8,7 +8,7 @@ class DAOUsuario:
     _REGISTRAR = 'INSERT INTO usuarios (name_user, pass_user) VALUES (%s, %s)'
     _VERIFICAR_USER_REGISTRO = 'SELECT * FROM usuarios WHERE name_user = %s'
     _LOGEAR = 'SELECT id_user FROM usuarios WHERE name_user = %s AND pass_user = %s'
-    _VERIFICAR_USER = 'SELECT is_admin FROM usuarios WHERE name_user = %s'
+    _VERIFICAR_ADMIN_USER = 'SELECT is_admin FROM usuarios WHERE name_user = %s'
 
     # Sentencias de escritura deben abrir la conexion y luego usar el cursor, no usar el cursor del metodo de clase
     # de clase database.py
@@ -20,6 +20,7 @@ class DAOUsuario:
                 valor_usuario = (usuario.name_user,)
                 cursor.execute(cls._VERIFICAR_USER_REGISTRO, valor_usuario)
                 if cursor.fetchone() is not None:
+                    print(cursor.fetchone())
                     print('El usuario ya existe, escoja otro.')
                     log.error(f'Error: El usuario {usuario.name_user} ya existe, escoja otro.')
                     registro_exitoso = False
@@ -27,27 +28,44 @@ class DAOUsuario:
                     valor_registro = (usuario.name_user, usuario.pass_user)
                     cursor.execute(cls._REGISTRAR, valor_registro)
                     print('Usuario registrado correctamente')
-                    registro = cursor.rowcount()
-                    log.debug(f'Se registro {registro} correctamente: {usuario.name_user}')
+                    log.debug(f'Un usuario se registro correctamente: {usuario.name_user}')
                     registro_exitoso = True
             return registro_exitoso
 
     @classmethod
     def logearUsuario(cls, usuario):
-        inicio_exitoso = None
         with Database.crearCursor() as cursor:
+            inicio_exitoso = False
             valores_usuario = (usuario.name_user, usuario.pass_user)
             cursor.execute(cls._LOGEAR, valores_usuario)
-            if cursor.fetchone():
+            verificacion = cursor.fetchone()
+            if verificacion:
+                id_usuario = verificacion[0]
                 inicio_exitoso = True
                 print('Inicio de sesion exitoso.')
-                log.debug(f'El usuario {usuario.name_user} ha iniciado sesion.')
-            else:
-                inicio_exitoso = False
-                print('Credenciales invalidas.')
-                log.error(f'Error: Credenciales invalidas. Usuario: {usuario.name_user} Contrasena: {usuario.pass_user}')
-        return inicio_exitoso
+                log.debug(f'El usuario {usuario.name_user} con ID: {id_usuario} ha iniciado sesion.')
+                return inicio_exitoso
+        #     else:
+        #         inicio_exitoso = False
+        #         print('Credenciales invalidas.')
+        #         log.error(f'Error: Credenciales invalidas. Usuario: {usuario.name_user} Contrasena: {usuario.pass_user}')
+        # return inicio_exitoso
 
+    @classmethod
+    def esAdministrador(cls, usuario):
+        # esAdmin = False
+        with Database.conectarDb() as conexion:
+            with conexion.cursor() as cursor:
+                valores_usuario_admin = (usuario.name_user,)
+                cursor.execute(cls._VERIFICAR_ADMIN_USER, valores_usuario_admin)
+                valor_admin = cursor.fetchone()
+                if valor_admin[0] == 'S':
+                    esAdmin = True
+                    # print('Es admin')
+                else:
+                    esAdmin = False
+                    # print('No es admin')
+            return esAdmin
 
 
 # print('Registro de usuario')
@@ -63,3 +81,8 @@ class DAOUsuario:
 #
 # user2 = Usuario(name_user=usuario1, pass_user=password1)
 # inicio = DAOUsuario.logearUsuario(user2)
+
+# print('Verificar usuario admin')
+# usuario2 = input('Ingrese el usuario')
+# user3 = Usuario(name_user=usuario2)
+# adminVer = DAOUsuario.esAdministrador(user3)
