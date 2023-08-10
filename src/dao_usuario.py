dc4c5d07f8214542904d9dceda903f71add55e35
Hardnessandextreme <86927718+Hardnessandextreme@users.noreply.github.com>
2023-08-10 00:30:57 -0500

    se ha agregado el metodo de clase logearUsuario

--- src/dao_usuario.py
+++ src/dao_usuario.py
@@ -21,20 +21,45 @@ class DAOUsuario:
                 cursor.execute(cls._VERIFICAR_USER_REGISTRO, valor_usuario)
                 if cursor.fetchone() is not None:
                     print('El usuario ya existe, escoja otro.')
-                    log.error('El usuario ya existe, escoja otro.')
+                    log.error(f'Error: El usuario {usuario.name_user} ya existe, escoja otro.')
                     registro_exitoso = False
                 else:
                     valor_registro = (usuario.name_user, usuario.pass_user)
                     cursor.execute(cls._REGISTRAR, valor_registro)
                     print('Usuario registrado correctamente')
-                    log.debug('Usuario registrado correctamente')
+                    registro = cursor.rowcount()
+                    log.debug(f'Se registro {registro} correctamente: {usuario.name_user}')
                     registro_exitoso = True
             return registro_exitoso
 
+    @classmethod
+    def logearUsuario(cls, usuario):
+        inicio_exitoso = None
+        with Database.crearCursor() as cursor:
+            valores_usuario = (usuario.name_user, usuario.pass_user)
+            cursor.execute(cls._LOGEAR, valores_usuario)
+            if cursor.fetchone():
+                inicio_exitoso = True
+                print('Inicio de sesion exitoso.')
+                log.debug(f'El usuario {usuario.name_user} ha iniciado sesion.')
+            else:
+                inicio_exitoso = False
+                print('Credenciales invalidas.')
+                log.error(f'Error: Credenciales invalidas. Usuario: {usuario.name_user} Contrasena: {usuario.pass_user}')
+        return inicio_exitoso
 
 
-print('Registro de usuario')
-usuario = input('Ingrese el usuario: ')
-password = input('Ingrese la contrasenia: ')
-user1 = Usuario(name_user=usuario, pass_user=password)
-registro = DAOUsuario.registrarUsuario(user1)
+
+# print('Registro de usuario')
+# usuario = input('Ingrese el usuario: ')
+# password = input('Ingrese la contrasenia: ')
+# user1 = Usuario(name_user=usuario, pass_user=password)
+# registro = DAOUsuario.registrarUsuario(user1)
+
+
+# print('Inicio de sesion')
+# usuario1 = input('Ingrese el usuario: ')
+# password1 = input('Ingrese la contrasenia: ')
+#
+# user2 = Usuario(name_user=usuario1, pass_user=password1)
+# inicio = DAOUsuario.logearUsuario(user2)
